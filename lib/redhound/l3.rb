d379be0bdc2c9bf44f24bb4e48cbda79d30de8be
ydah <t.yudai92@gmail.com>
2025-01-17 14:33:13 +0900

    Fix an NameError

--- lib/redhound/l3.rb
+++ lib/redhound/l3.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
-require_relative 'l3/arp'
 require_relative 'l3/base'
+
+require_relative 'l3/arp'
 require_relative 'l3/ipv4'
 require_relative 'l3/ipv6'
 require_relative 'l3/protocol'
